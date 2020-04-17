ee4169ccbc6f07f985936a4a5f8e83dada1e291e
Ilknur Mustafov <ilknurilko@gmail.com>
2020-04-17 13:13:17 +0200

    Add Search & Video Details

---
+++ src/components/VideoList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Grid } from '@material-ui/core';
+import VideoItem from "./VideoItem";
+
+const VideoList = ({videos}) => {
+    const listOfVideos = videos.map((video) => 
+        <VideoItem />)
+    return listOfVideos;
+}
+
+export default VideoList;
